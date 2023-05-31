ac59668a4ad9f9f87fdcfa1acb904472f1014d68
SaadEssam <SaadEssam059@gmail.com>
2023-05-31 18:22:51 +0300

    Add Meta tags

--- pages/404.tsx
+++ pages/404.tsx
@@ -8,13 +8,20 @@ const Error = () => {
     <>
       <Header />
       <div className=" flex h-screen flex-col items-center justify-center gap-6">
+        <Head>
+          <title>Page Not Found - Medium</title>
+          <meta name="description" content="Page Not Found" />
+        </Head>
         <h1 className="bg-gradient-to-br from-green-600 to-green-900 bg-clip-text font-semibold text-transparent sm:text-9xl md:text-[300px]">
           404
         </h1>
         <h3 className="text-xl font-semibold">
           Sorry, but this page does not exist.
         </h3>
-        <Link href="/" className="py-3 px-5 rounded-full bg-green-700 hover:bg-green-600 transition-all text-white mt-0">
+        <Link
+          href="/"
+          className="mt-0 rounded-full bg-green-700 px-5 py-3 text-white transition-all hover:bg-green-600"
+        >
           Homepage
         </Link>
       </div>
